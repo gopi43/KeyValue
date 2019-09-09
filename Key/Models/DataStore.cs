50e83bce862b07589bda3970b12b00c5a18b0338
Gopi nath <gopi439@outlook.com>
2019-09-09 09:42:33 +0530

    Inotoal

---
+++ Key/Models/DataStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace Key.Models
+{
+    public class DataStore
+    {
+        public string Key { get; set; }
+        public Json Value { get; set; }
+    }
+}
